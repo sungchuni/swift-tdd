4ab649e30044d92a0413b37260d7d5887eb34c35
Sonny, 성천 <new.sungchuni@gmail.com>
2021-07-22 00:29:10 +0900

    spec: chap 11

--- sonny-swift-tdd/Money.swift
+++ sonny-swift-tdd/Money.swift
@@ -17,11 +17,11 @@ class Money: Equatable {
     static func == (lhs: Money, rhs: Money) -> Bool {
         return lhs.amount == rhs.amount && lhs.currency == rhs.currency
     }
-    static func dollar(_ amount: Int) -> Dollar {
-        return Dollar(amount, "USD")
+    static func dollar(_ amount: Int) -> Money {
+        return Money(amount, "USD")
     }
-    static func franc(_ amount: Int) -> Franc {
-        return Franc(amount, "CHF")
+    static func franc(_ amount: Int) -> Money {
+        return Money(amount, "CHF")
     }
     func times(_ multiplier: Int) -> Money {
         return Money(amount * multiplier, currency)
--- sonny-swift-tddTests/sonny_swift_tddTests.swift
+++ sonny-swift-tddTests/sonny_swift_tddTests.swift
@@ -14,23 +14,13 @@ class sonny_swift_tddTests: XCTestCase {
         XCTAssertEqual(Money.dollar(10), five.times(2))
         XCTAssertEqual(Money.dollar(15), five.times(3))
     }
-    func testFrancMultiplication() throws {
-        let five = Money.franc(5)
-        XCTAssertEqual(Money.franc(10), five.times(2))
-        XCTAssertEqual(Money.franc(15), five.times(3))
-    }
     func testEquality() throws {
         XCTAssertEqual(Money.dollar(5), Money.dollar(5))
         XCTAssertNotEqual(Money.dollar(5), Money.dollar(6))
-        XCTAssertEqual(Money.franc(5), Money.franc(5))
-        XCTAssertNotEqual(Money.franc(5), Money.franc(6))
         XCTAssertNotEqual(Money.dollar(5), Money.franc(5))
     }
     func testCurrency() throws {
         XCTAssertEqual("USD", Money.dollar(1).currency)
         XCTAssertEqual("CHF", Money.franc(1).currency)
-    }
-    func testDifferentClassEquality() {
-        XCTAssertEqual(Money(10, "CHF"), Franc(10, "CHF"))
     }
 }
